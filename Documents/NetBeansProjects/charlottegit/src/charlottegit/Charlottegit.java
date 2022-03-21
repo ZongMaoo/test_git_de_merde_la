69980fd04260c9611716123a3458f0eba7d4d35c
ZongMaoo <denisn2505@gmail.com>
2022-03-21 18:47:41 +0100

    charlotte c'est chelou

---
+++ Documents/NetBeansProjects/charlottegit/src/charlottegit/Charlottegit.java
@@ -0,0 +1,16 @@
+package charlottegit;
+
+/**
+ *
+ * @author denis
+ */
+public class Charlottegit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
